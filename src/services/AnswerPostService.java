3c4b5bb48902b4af94fcfca2085a4a1e7be5f375
Elyes <chebbaelyes@gmail.com>
2018-02-09 21:16:12 +0100

    made create in AnswerPostService turned Member Owner to int ownerId in class Post

--- src/services/AnswerPostService.java
+++ src/services/AnswerPostService.java
@@ -4,10 +4,15 @@ import iservice.Create;
 import iservice.Delete;
 import iservice.Read;
 import iservice.Update;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import models.AnswerPost;
+import models.Enumerations;
+import models.Enumerations.PostType;
 
-public class AnswerPostService extends Service implements Create<AnswerPostService>,Delete<AnswerPostService>,Read<AnswerPostService>,Update<AnswerPostService>{
+public class AnswerPostService extends Service implements Create<AnswerPost>,Delete<AnswerPost>,Read<AnswerPost>,Update<AnswerPost>{
 
     private static AnswerPostService answerPostService;
     
@@ -23,27 +28,55 @@ public class AnswerPostService extends Service implements Create<AnswerPostServi
     }
 
     @Override
-    public AnswerPostService create(AnswerPostService obj) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public AnswerPost create(AnswerPost obj) throws SQLException {
+	/*
+	* Insertion into "post" table.
+	*/
+	String req = "INSERT INTO `post`(`date`, `content`, `type`, `user_id`) VALUES (?,?,?,?)";
+	PreparedStatement pst = CONNECTION.prepareStatement(req);
+	pst.setTimestamp(1, obj.getDate());
+	pst.setString(2, "");
+	pst.setInt(3, PostType.ANSWER.ordinal());
+	pst.setInt(4, obj.getId());
+	pst.executeUpdate();
+	/*
+	* Insertion into "answerpost" table.
+	*/
+	req = "INSERT INTO `post_answer`(`post_id`, `answer_id`) VALUES (?,?)";
+	pst = CONNECTION.prepareStatement(req);
+	pst.setInt(1, obj.getId());
+	pst.setInt(2, obj.getAnswerId());
+	pst.executeUpdate();
+	/*
+	* Retrieval of new AnswerPost id.
+	*/
+	req = "SELECT MAX(id) FROM `post_answer`";
+	pst = CONNECTION.prepareStatement(req);
+	ResultSet rs = pst.executeQuery();
+	rs.next();
+	obj.setId(rs.getInt("id"));
+	return obj;
     }
 
     @Override
-    public void delete(AnswerPostService obj) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void delete(AnswerPost obj) throws SQLException {
+	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public AnswerPostService get(AnswerPostService obj) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public AnswerPost get(AnswerPost obj) throws SQLException {
+	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public List<AnswerPostService> getAll(AnswerPostService obj) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public List<AnswerPost> getAll(AnswerPost obj) throws SQLException {
+	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public AnswerPostService update(AnswerPostService obj) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public AnswerPost update(AnswerPost obj) throws SQLException {
+	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+
+    
 }
