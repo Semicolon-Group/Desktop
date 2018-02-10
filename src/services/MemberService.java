cbea8b4f404cebe6a6e8c824a0747e676a27aed8
badis <badis@BDAS>
2018-02-10 15:01:11 +0100

    badis cruds done , except conversation which will be consulted with lays

--- src/services/MemberService.java
+++ src/services/MemberService.java
@@ -8,27 +8,36 @@ package services;
 import iservice.Create;
 import iservice.Read;
 import iservice.Update;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.sql.Timestamp;
 import java.util.Date;
 import java.util.List;
+import models.Address;
+import models.Enumerations;
 import models.Member;
 
 /**
  *
  * @author Elyes
  */
-public class MemberService extends Service implements Create<Member>,Update<Member>,Read<Member>{
+public class MemberService extends Service implements Create<Member>, Update<Member>, Read<Member> {
 
     private static MemberService memberService;
-    
-    private MemberService(){
+    Statement st;
+    Connection con;
+    PreparedStatement pst;
+    ResultSet rs;
+
+    private MemberService() {
         super();
     }
-    
-    public static MemberService getInstance(){
-        if(memberService == null){
+
+    public static MemberService getInstance() {
+        if (memberService == null) {
             return memberService = new MemberService();
         }
         return memberService;
@@ -36,7 +45,7 @@ public class MemberService extends Service implements Create<Member>,Update<Memb
 
     @Override
     public Member create(Member obj) throws SQLException {
-	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
@@ -75,13 +84,50 @@ public class MemberService extends Service implements Create<Member>,Update<Memb
 
     @Override
     public Member get(Member obj) throws SQLException {
-	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        String condition = "";
+        if (obj.getId() != 0) {
+            condition = "Where id = " + obj.getId();
+        } else if (obj.getPseudo() != null) {
+            condition = "Where pseudo = " + obj.getPseudo();
+        } else if (obj.getEmail() != null) {
+            condition = "Where email = " + obj.getEmail();
+        }
+        String req = "Select * , date_format( birth_date ,'%d %m %Y') as birth from user " + condition;
+        st = CONNECTION.createStatement();
+        rs = st.executeQuery(req);
+        rs.next();
+        obj.setPseudo(rs.getString("pseudo"));
+        obj.setFirstname(rs.getString("firstname"));
+        obj.setLastname(rs.getString("lastname"));
+        obj.setEmail(rs.getString("email"));
+        obj.setPassword(rs.getString("password"));
+        obj.setBirthDate(rs.getDate("birth_date"));
+        obj.setGender(rs.getBoolean("gender"));
+        obj.setHeight(rs.getFloat("height"));
+        obj.setBodyType(Enumerations.BodyType.values()[rs.getInt("body_type")]);
+        obj.setChildrenNumber(rs.getInt("children_number"));
+        obj.setReligion(Enumerations.Religion.values()[rs.getInt("relegion")]);
+        obj.setReligionImportance(Enumerations.Importance.values()[rs.getInt("relegion_importance")]);
+        obj.setSmoker(rs.getBoolean("smoker"));
+        obj.setDrinker(rs.getBoolean("drinker"));
+        obj.setMinAge(rs.getInt("min_age"));
+        obj.setMaxAge(rs.getInt("max_age"));
+        obj.setProximity(Enumerations.Proximity.values()[rs.getInt("proximity")]);
+        obj.setLastLogin(rs.getTimestamp("last_login"));
+        obj.setLocked(rs.getShort("locked"));
+        obj.setIp(rs.getString("ip"));
+        obj.setPort(rs.getInt("port"));
+        obj.setAddress(new Address(obj.getId()));
+        
+        
+        
+        return obj;
     }
 
     @Override
     public List<Member> getAll(Member obj) throws SQLException {
-	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    
 }
---
+++ test/MainTest.java
@@ -0,0 +1,110 @@
+
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
+import models.Conversation;
+import models.Member;
+import models.Message;
+import models.User;
+import services.MemberService;
+import services.MessageService;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author badis
+ */
+public class MainTest {
+
+    public static void main(String[] args) throws SQLException {
+//        getMembreTest(); ci bon
+//          getAllMessageTest(); ci bon
+        //       createMessageTest(); ci bon
+
+// getAllMessageTest(); ci bon
+//        getMsgTest(); ci bon 
+//            updateMsg(); ci bon
+
+    }
+
+    public static void getMsgTest() throws SQLException {
+        MessageService messageService = MessageService.getInstance();
+        Member sender = new Member();
+        sender.setId(1);
+        Member receiver = new Member();
+        receiver.setId(2);
+        Message m = new Message();
+        m.setId(11);
+        m.setContent(m.getContent());
+        m.setSenderId(1);
+        m.setReceiverId(2);
+        Message m2 = messageService.get(m);
+        System.out.println(m2.getId() + " " + m2.getContent() + " " + m2.isSeen() + " " + m2.getSeenDate() + " " + m2.getSenderId() + " " + m2.getReceiverId());
+        //messages.forEach(m2
+        //      -> System.out.println(m2.getId() + " " + m2.getContent() + " " + m2.isSeen() + " " + m2.getSeenDate() + " " + m2.getSenderId() + " " + m2.getReceiverId())
+        //id	content	seen	seen_date	date	sender_id	receiver_id
+
+    }
+
+    public static void getAllMessageTest() throws SQLException {
+        MessageService messageService = MessageService.getInstance();
+        Member sender = new Member();
+        sender.setId(1);
+        Member receiver = new Member();
+        receiver.setId(2);
+        Message m = new Message();
+        m.setSenderId(sender.getId());
+        m.setReceiverId(receiver.getId());
+        List<Message> messages = messageService.getAll(m);
+        messages.forEach(m2
+                -> System.out.println(m2.getId() + " " + m2.getContent() + " " + m2.isSeen() + " " + m2.getSeenDate() + " " + m2.getSenderId() + " " + m2.getReceiverId())
+        );//id	content	seen	seen_date	date	sender_id	receiver_id
+
+    }
+
+    public static void getMembreTest() throws SQLException {
+        Member m = new Member();
+        m.setId(1);
+        MemberService memberService = MemberService.getInstance();
+        m = memberService.get(m);
+        System.out.println(m.getId() + " " + m.getPseudo() + " " + m.getFirstname() + " " + m.getLastname() + " " + m.getEmail() + " " + m.getPassword() + " " + m.getBirthDate()
+                + " " + m.isGender() + " " + m.getHeight() + " " + m.getHeight() + " " + m.getBodyType() + " " + m.getChildrenNumber() + " " + m.getReligion() + " " + m.getReligionImportance() + " " + m.isSmoker() + " "
+                + m.isDrinker() + " " + m.getMaxAge() + " " + m.getMinAge() + " " + m.getProximity()
+                + " " + m.getLastLogin() + " " + m.getLocked() + " " + m.getIp() + " " + m.getPort() + " " + m.getPreferedRelations() + " " + m.getPreferedStatuses());
+    }
+
+    public static void createMessageTest() throws SQLException {
+        MessageService msg1 = MessageService.getInstance();
+        User sender = new Member();
+        sender.setId(1);
+        User receiver = new Member();
+        receiver.setId(2);
+        
+        
+        //    public Message(String content, Timestamp date, int senderId, int receiverId, boolean seen, Timestamp seenDate) {
+
+        Message msg = new Message("yoo2", new Timestamp(System.currentTimeMillis()), 2, 1, false, new Timestamp(System.currentTimeMillis()));
+        msg = msg1.create(msg);
+        System.out.println(msg.getId() + " " + msg.getContent() + " " + msg.getSeenDate());
+
+    }
+
+    public static void updateMsg() throws SQLException {
+         MessageService msg1 = MessageService.getInstance();
+         Message msg = new Message();
+         msg.setId(11);
+         msg.setSeenDate(new Timestamp(System.currentTimeMillis()));
+         msg.setSeen(true);
+         msg1.update(msg);
+        
+       
+        
+        
+
+    }
+}
