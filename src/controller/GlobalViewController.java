196c2a42e1172128ab91e3ccbcf53e901c83d64c
SeifAbdennadher <m.abdennadher.seif@gmail.com>
2018-02-13 20:26:11 +0100

    fixing navbar positionning

--- src/controller/GlobalViewController.java
+++ src/controller/GlobalViewController.java
@@ -15,7 +15,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
@@ -29,28 +31,33 @@ import javafx.scene.text.Font;
 public class GlobalViewController implements Initializable {
 
     @FXML
-    private ScrollPane content;
+    private AnchorPane mainAnchor;
+    @FXML
+    private VBox contentVBox;
     @FXML
     private AnchorPane navBar;
     @FXML
-    private AnchorPane mainAnchor;
+    private ScrollPane scroll;
+    @FXML
+    private AnchorPane content;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        Font.loadFont(getClass().getResourceAsStream("/assets/fonts/fontawesome-webfont3e6e.ttf"), 14);
+        scroll.vvalueProperty().addListener( (observable, oldValue, newValue) -> {
+            double yTranslate = (content.getHeight()*newValue.doubleValue())-(scroll.getHeight()*newValue.doubleValue());
+            navBar.translateYProperty().setValue(yTranslate);
+        });
         // TODO
     }
     
     private void setContent(String path){
         try {
-            VBox newLoadedPane =  FXMLLoader.load(getClass().getResource(path));
-            content.setContent(newLoadedPane);
-//            content.getChildren().clear();
-//            content.getChildren().add(newLoadedPane);
-            
+            Pane newLoadedPane =  FXMLLoader.load(getClass().getResource(path));
+            VBox.setVgrow(scroll, Priority.ALWAYS);
+            content.getChildren().add(newLoadedPane);
             newLoadedPane.prefWidthProperty().bind(content.widthProperty());
             newLoadedPane.prefHeightProperty().bind(content.heightProperty());
         } catch (IOException ex) {
