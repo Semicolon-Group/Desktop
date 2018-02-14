7d5c366cf4c4c1a8b75c10c1d70de801887afe6b
SeifAbdennadher <m.abdennadher.seif@gmail.com>
2018-02-14 13:34:35 +0100

    Fixing some icons issues

--- src/controller/GlobalViewController.java
+++ src/controller/GlobalViewController.java
@@ -5,9 +5,11 @@
  */
 package controller;
 
+import com.sun.javafx.property.adapter.PropertyDescriptor;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -16,6 +18,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
@@ -76,41 +79,88 @@ public class GlobalViewController implements Initializable {
     private VBox recommandationBox;
     @FXML
     private VBox blindDateBox;
+    
+    private EventHandler notificationPaneHandler = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent event) {
+            String id = ((Node)event.getTarget()).getId();
+            if(id != null && (id.equals("notification_icon") || id.equals("notification_icon_box"))){
+                notificationPane.setVisible(!notificationPane.isVisible());
+                notificationPane.setPrefHeight(600);
+                conversationPane.setVisible(false);
+                conversationPane.setPrefHeight(0);
+                accountPane.setVisible(false);
+                accountPane.setPrefHeight(0);
+            }else if(id != null && (id.equals("message_icon") || id.equals("message_icon_box"))){
+                conversationPane.setVisible(!conversationPane.isVisible());
+                conversationPane.setPrefHeight(600);
+                notificationPane.setVisible(false);
+                notificationPane.setPrefHeight(0);
+                accountPane.setVisible(false);
+                accountPane.setPrefHeight(0);
+            }else if(id != null && (id.equals("account_icon") || id.equals("account_icon_box"))){
+                accountPane.setVisible(!accountPane.isVisible());
+                accountPane.setPrefHeight(600);
+                conversationPane.setVisible(false);
+                conversationPane.setPrefHeight(0);
+                notificationPane.setVisible(false);
+                notificationPane.setPrefHeight(0);
+            }else{
+                if(!((Node)event.getTarget()).getStyleClass().contains(new String("notificationClickable"))){
+                notificationPane.setVisible(false);
+                notificationPane.setPrefHeight(0);
+                }
+                if(!((Node)event.getTarget()).getStyleClass().contains(new String("conversationClickable"))){
+                    conversationPane.setVisible(false);
+                    conversationPane.setPrefHeight(0);
+                }
+                if(!((Node)event.getTarget()).getStyleClass().contains(new String("accountClickable"))){
+                    accountPane.setVisible(false);
+                    accountPane.setPrefHeight(0);
+                }
+            }
+        }
+    };
+    @FXML
+    private ImageView accountIcon;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        mainAnchor.addEventFilter(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                String id = ((Node)event.getTarget()).getId();
-                if(id==null) return;
-                if(id.equals("notification_icon") || id.equals("notification_icon_box")){
-                    notificationPane.setVisible(true);
-                    conversationPane.setVisible(false);
-                    accountPane.setVisible(false);
-                }else if(id.equals("message_icon") || id.equals("message_icon_box")){
-                    conversationPane.setVisible(true);
-                    notificationPane.setVisible(false);
-                    accountPane.setVisible(false);
-                }else if(id.equals("account_icon") || id.equals("account_icon_box")){
-                    accountPane.setVisible(true);
-                    conversationPane.setVisible(false);
-                    notificationPane.setVisible(false);
-                }else{
-                    notificationPane.setVisible(false);
-                    conversationPane.setVisible(false);
-                    accountPane.setVisible(false);
-                }
-            }
-        });
+        mainAnchor.addEventFilter(MouseEvent.MOUSE_CLICKED, notificationPaneHandler);
         scroll.vvalueProperty().addListener( (observable, oldValue, newValue) -> {
             double yTranslate = (content.getHeight()*newValue.doubleValue())-(scroll.getHeight()*newValue.doubleValue());
             navBar.translateYProperty().setValue(yTranslate);
         });
-        // TODO
+        notificationPane.visibleProperty().addListener((observable, oldValue, newValue) -> {
+            if(newValue){
+                notificationIcon.getStyleClass().remove(notificationIcon.getStyleClass().size()-1);
+                activeIcon(notificationIcon, "notification");
+                return;
+            }
+            notificationIcon.getStyleClass().add("hoverable");
+            releaseIcon(notificationIcon, "notification");
+        });
+        conversationPane.visibleProperty().addListener((observable, oldValue, newValue) -> {
+            if(newValue){
+                messageIcon.getStyleClass().remove(messageIcon.getStyleClass().size()-1);
+                activeIcon(messageIcon, "message");
+                return;
+            }
+            messageIcon.getStyleClass().add("hoverable");
+            releaseIcon(messageIcon, "message");
+        });
+        accountPane.visibleProperty().addListener((observable, oldValue, newValue) -> {
+            if(newValue){
+                accountIcon.getStyleClass().remove(accountIcon.getStyleClass().size()-1);
+                activeIcon(accountIcon, "account");
+                return;
+            }
+            accountIcon.getStyleClass().add("hoverable");
+            releaseIcon(accountIcon, "account");
+        });
     }
     
     private void setContent(String path){
@@ -170,5 +220,33 @@ public class GlobalViewController implements Initializable {
         quickSearchBox.setId("");
         blindDateBox.setId("");
         recommandationBox.setId("selected");
+    }
+
+    @FXML
+    private void hover(MouseEvent event) {
+        Pane target = ((Pane)event.getTarget());
+        ImageView icon = ((ImageView)target.getChildren().get(0));
+        if(!icon.getStyleClass().contains(new String("hoverable"))) return;
+        String id = target.getId();
+        activeIcon(icon, id.substring(0, id.indexOf("_")));
+    }
+
+    @FXML
+    private void exit(MouseEvent event) {
+        Pane target = ((Pane)event.getTarget());
+        ImageView icon = ((ImageView)target.getChildren().get(0));
+        if(!icon.getStyleClass().contains(new String("hoverable"))) return;
+        String id = target.getId();
+        releaseIcon(icon, id.substring(0, id.indexOf("_")));
+    }
+    
+    private void activeIcon(ImageView imageView, String imageName){
+        String url = getClass().getResource("/view/assets/icons/hovered/"+imageName+".png").toExternalForm();
+        imageView.setImage(new Image(url));
+    }
+    
+    private void releaseIcon(ImageView imageView, String imageName){
+        String url = getClass().getResource("/view/assets/icons/natural/"+imageName+".png").toExternalForm();
+        imageView.setImage(new Image(url));
     }
 }
