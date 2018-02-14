050b45356d77eb1727981421d7e33ce0550ddc56
Elyes <chebbaelyes@gmail.com>
2018-02-14 20:54:12 +0100

    created postTemplate fxml testing it with homeView

--- src/controller/HomeViewController.java
+++ src/controller/HomeViewController.java
@@ -5,9 +5,16 @@
  */
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.layout.VBox;
 
 /**
  * FXML Controller class
@@ -16,12 +23,27 @@ import javafx.fxml.Initializable;
  */
 public class HomeViewController implements Initializable {
 
+    @FXML
+    private VBox feed;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        try {
+            Parent post = FXMLLoader.load(getClass().getResource("/view/postTemplate.fxml"));
+            feed.getChildren().add(post);
+            Parent post1 = FXMLLoader.load(getClass().getResource("/view/postTemplate.fxml"));
+            feed.getChildren().add(post1);
+            Parent post2 = FXMLLoader.load(getClass().getResource("/view/postTemplate.fxml"));
+            feed.getChildren().add(post2);
+            Parent post3 = FXMLLoader.load(getClass().getResource("/view/postTemplate.fxml"));
+            feed.getChildren().add(post3);
+            
+        } catch (IOException ex) {
+            Logger.getLogger(HomeViewController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }    
     
 }
---
+++ src/controller/PostTemplateController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+
+/**
+ * FXML Controller class
+ *
+ * @author Elyes
+ */
+public class PostTemplateController implements Initializable {
+
+    @FXML
+    private VBox post;
+    @FXML
+    private ImageView photo;
+    @FXML
+    private Label name;
+    @FXML
+    private Label time;
+    @FXML
+    private TextArea content;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
