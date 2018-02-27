c97863634cd68aa80ab6f63761a0bdfddf44ff84
SeifAbdennadher <m.abdennadher.seif@gmail.com>
2018-02-27 17:00:46 +0100

    Revert "Achref final branch"

--- src/controller/InsViewController.java
+++ src/controller/InsViewController.java
@@ -7,21 +7,14 @@ package controller;
 
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
-import com.restfb.DefaultFacebookClient;
-import com.restfb.FacebookClient;
-import com.restfb.Version;
-import com.restfb.scope.FacebookPermissions;
-import com.restfb.scope.ScopeBuilder;
-import com.restfb.types.User;
 import static controller.MainAchref.container3;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URL;
 import java.time.ZoneId;
 import static java.time.temporal.TemporalQueries.localDate;
 import java.util.Date;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.animation.RotateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -36,11 +29,10 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.util.Duration;
+import javax.mail.MessagingException;
 import models.Email;
 import models.Member;
-import org.json.JSONObject;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import util.SendMail;
 
 
 /**
@@ -90,14 +82,14 @@ public class InsViewController implements Initializable {
     @FXML
     private ImageView image;
     @FXML
-    private JFXTextField emailTextField;
+    private JFXTextField Email;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        RotateTransition rt = new RotateTransition(Duration.seconds(120),image);
+        RotateTransition rt = new RotateTransition(Duration.seconds(35),image);
         rt.setByAngle(9*360);
         rt.play();
 
@@ -113,7 +105,6 @@ public class InsViewController implements Initializable {
         } else {
             labelfirst.setText("");
         }
-
         if (Last_name.getText().equals("")) {
             labelname.setText("Field is empty !");
             labelname.setVisible(true);
@@ -121,7 +112,6 @@ public class InsViewController implements Initializable {
         } else {
             labelname.setText("");
         }
-
         if (pseudo.getText().equals("")) {
             labelps.setText("Field is empty !");
             labelps.setVisible(true);
@@ -129,20 +119,18 @@ public class InsViewController implements Initializable {
         } else {
             labelps.setText("");
         }
-        if (emailTextField.getText().equals("")) {
+        if (Email.getText().equals("")) {
             labelemail.setText("Field is empty !");
             labelemail.setVisible(true);
             valid = false;
         }
-
-        if (!emailTextField.getText().contains("@") && !emailTextField.getText().contains(".")) {
+        if (!Email.getText().contains("@") && !Email.getText().contains(".")) {
             labelemail.setText("E-mail is not valid !");
             labelemail.setVisible(true);
             valid = false;
         } else {
             labelemail.setText("");
         }
-
         if (Password.getText().equals("")) {
             labelpass.setText("Field is empty !");
             labelpass.setVisible(true);
@@ -150,13 +138,11 @@ public class InsViewController implements Initializable {
         } else {
             labelpass.setText("");
         }
-
         if (Rpassword.getText().equals("")) {
             labelrepass.setText("Field is empty !");
             labelrepass.setVisible(true);
             valid = false;
         }
-
         if (!Password.getText().equals(Rpassword.getText())) {
             labelrepass.setText("Password doesn't match !");
             labelrepass.setVisible(true);
@@ -166,31 +152,24 @@ public class InsViewController implements Initializable {
             labelrepass.setTextFill(Color.web("GREEN"));;
             labelrepass.setVisible(true);
         }
-
         if (birth_date.getValue() == null) {
             labeldate.setText("Field is empty !");
             labeldate.setVisible(true);
             valid = false;
-        }
+            }
         if(!valid) return;
-        
         m = new Member();
         m.setFirstname(Firstname.getText());
         m.setLastname(Last_name.getText());
         m.setPseudo(pseudo.getText());
-        m.setEmail(emailTextField.getText());
+        m.setEmail(Email.getText());
         m.setPassword(Password.getText());
         m.setGender(male.isSelected());
         m.setBirthDate(java.sql.Date.valueOf(birth_date.getValue()));
-        
-
-
         FXMLLoader loader = container3.switchView("InscriptionDetailsView");
-        System.out.println(loader);
         ((InscriptionDetailsViewController)loader.getController()).setMember(m);
+
         
-         
-            
     }
 
     @FXML
@@ -199,110 +178,10 @@ public class InsViewController implements Initializable {
         Firstname.clear();
         Last_name.clear();
         pseudo.clear();
-        emailTextField.clear();
+        Email.clear();
         Password.clear();
         Rpassword.clear();
         
-
-    }
-
-    @FXML
-    private void fbAuth(ActionEvent event) {
-         
-        String appId = "160006897984731";
-        String appSecretKey = "490b349f7387626f63ea4b4e2b3d2c2f";
-        String domain = "http://whitedisplay.com/";
-        String code="";
-        String userAccessToken="";
-        
-        //permission lel email
-        ScopeBuilder scopeBuilder = new ScopeBuilder();
-        scopeBuilder.addPermission(FacebookPermissions.EMAIL);
-        scopeBuilder.addPermission(FacebookPermissions.USER_LOCATION);
-        scopeBuilder.addPermission(FacebookPermissions.USER_BIRTHDAY);
-        
-
-        //client jdid w nvl boite de dialg
-        FacebookClient client = new DefaultFacebookClient(Version.VERSION_2_6);
-        String loginDialogUrlString = client.getLoginDialogUrl(appId, domain, scopeBuilder);
-
-        //chome driver
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get(loginDialogUrlString);
-
-        /*while true bch tab9a tet3awed dima psk sans l while yodkhel lel if une seule fois
-        w it ll get the currentUrl only once*/
-        while (true) {
-            //ken redirect w c bon 
-            //w driver.title mch .getCurrentUrl psk yrajja3 fi valeur ghalta
-            if (driver.getTitle().contains("White screen page")) {
-                String url = driver.getCurrentUrl();
-
-                //puisque jed om el accessToken mafamech rihtou w ena nest7a9ou donc bch ngahfou mel url
-                //w puisque l url mefhch ken l tokes akahw donc bch n'split el url win yabda si l code
-                if (url.contains("code=") && "".equals(code) ) {
-                    String[] parts = url.split("code=");
-                    String part1 = parts[0];
-                    String codeToken = parts[1];
-                    code = codeToken;
-                    System.out.println(codeToken);
-
-                     
-                      try {
-                        //khorm mtaa decryptage
-                         userAccessToken =  InsViewController.call_me(appId, domain, appSecretKey, codeToken);
-                       
-                         FacebookClient fbclient = new DefaultFacebookClient(userAccessToken);
-                         User user = fbclient.fetchObject("me",User.class);
-                         System.out.print("User Name = "+ user.getName());
-                         System.out.println("User birthday "+user.getBirthday());
-                         System.out.println("User email "+user.getEmail());
-                         System.out.println("User location "+user.getLocation());
-                         System.out.println("User id "+user.getId());
-
-                         
-                         
-                         Firstname.setText(user.getFirstName());
-                         Last_name.setText(user.getLastName());
-                         driver.quit();
-                      } catch (Exception e) {
-                        e.printStackTrace();
-                    }   
-                }
-            }
-        }
-    }
-    //decryptage taa l token eli sregtou ena 
-    //w puisque l output bel json donc voila
-    public static String call_me(String appId, String redirectUrl, String appSecret, String code) throws Exception {
-        String url = "https://graph.facebook.com/v2.12/oauth/access_token?"+"client_id=" + appId+"&redirect_uri=" + redirectUrl+"&client_secret=" + appSecret+"&code=" + code;
-        URL obj = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-        // optional default is GET
-        con.setRequestMethod("GET");
-        int responseCode = con.getResponseCode();
-        //System.out.println("\nSending 'GET' request to URL : " + url);
-        //System.out.println("Response Code : " + responseCode);
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-        }
-        in.close();
-
-        //print in String
-        //System.out.println(response.toString());
-
-        //Read JSON response and print
-        JSONObject myResponse = new JSONObject(response.toString());
-        System.out.println(myResponse.getString("access_token"));
-        
-        return(myResponse.getString("access_token"));
-        //System.out.println("result after Reading JSON Response");
-        //System.out.println("origin- "+myResponse.getString("origin"));
 
     }
 
